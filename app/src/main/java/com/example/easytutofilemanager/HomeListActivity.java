b3b194cb241ef19044cde5201d7c6b6b0fb296da
chonkee-dave76 <daveywavey31@outlook.com>
2023-04-19 10:27:17 +0800

    Folder creation xml

--- app/src/main/java/com/example/easytutofilemanager/HomeListActivity.java
+++ app/src/main/java/com/example/easytutofilemanager/HomeListActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.File;
@@ -17,8 +19,17 @@ public class HomeListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_file_list);
 
+        Button createNewFolder = findViewById(R.id.createNewFolder);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         TextView noFilesText = findViewById(R.id.nofiles_textview);
+
+        createNewFolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toFolderCreate = new Intent(HomeListActivity.this, NewFolderCreation.class);
+                startActivity(toFolderCreate);
+            }
+        });
 
         String path = getIntent().getStringExtra("path");
         File root = new File(path);
---
+++ app/src/main/java/com/example/easytutofilemanager/NewFolderCreation.java
@@ -0,0 +1,16 @@
+package com.example.easytutofilemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewFolderCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_folder_creation);
+
+
+    }
+}
