845c08dd6b0b1d1348632d63a37d70a7287e35dd
chonkee-dave76 <daveywavey31@outlook.com>
2023-04-19 10:38:31 +0800

    General Changes

--- app/src/main/java/com/example/easytutofilemanager/NewFolderCreation.java
+++ app/src/main/java/com/example/easytutofilemanager/NewFolderCreation.java
@@ -2,7 +2,11 @@ package com.example.easytutofilemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.cardemulation.HostNfcFService;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewFolderCreation extends AppCompatActivity {
 
